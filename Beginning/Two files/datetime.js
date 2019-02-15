7a68d02f48e9edb3378ea0ae01f57ab64376eeae
disbeEnriquePerez <33563417+disbeEnriquePerez@users.noreply.github.com>
2019-02-15 09:34:59 -0800

    basics of JavaScript

---
+++ Beginning/Two files/datetime.js
@@ -0,0 +1,11 @@
+var now = new Date();
+var localtime = now.toString();
+var utctime = now.toGMTString();
+document.write("<b>Local time:</b> " + localtime + "<BR>");
+document.write("<b>UTC time:</b> "+ utctime);
+var hours = now.getHours();
+var mins = now.getMinutes();
+var secs = now.getSeconds();
+document.write("<h1>");
+document.write(hours + ":" + mins + ":" + secs);
+document.write("</h1>");
